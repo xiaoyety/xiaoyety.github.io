6105d24a38ceb65882902e53c1edf2fe7624494f
xiaoyety <134197521+xiaoyety@users.noreply.github.com>
2024-02-15 23:48:09 +0800

    Add files via upload

--- js/Nav.js
+++ js/Nav.js
@@ -7,3 +7,22 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0"; 
     document.getElementById("main").style.marginLeft= "0";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
+  
+    if ("IntersectionObserver" in window) {
+      let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            lazyBackgroundObserver.unobserve(entry.target);
+          }
+        });
+      });
+  
+      lazyBackgrounds.forEach(function(lazyBackground) {
+        lazyBackgroundObserver.observe(lazyBackground);
+      });
+    }
+  });
